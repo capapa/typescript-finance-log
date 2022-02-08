91e0368c83619741b84e53c6b8940801d12e1711
RashRS <rash.rrs@gmail.com>
2022-02-08 15:00:58 +0500

    TS Alias

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,19 +1,5 @@
-let greet: Function;
+type StringOrNum = string | number;
 
-greet = () => {
-    console.log('hello!');
+const logDetails = (uid: StringOrNum) => {
+    console.log(uid)
 }
-
-const add = (a:number, b:number, c?: number | string, d: number = 10) => {
-    console.log(a + b);
-    console.log(c);
-    console.log(d);
-}
-
-add(5, 7);
-
-const minus = (a:number, b:number) => {
-    return a - b;
-}
-
-let result = minus(10, 2);
