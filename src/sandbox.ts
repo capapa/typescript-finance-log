bce9655271b4f3177b4de9133b2dc0a64a3c113e
RashRS <rash.rrs@gmail.com>
2022-02-08 15:12:46 +0500

    TS Function Sgignatures

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,5 +1,29 @@
-type StringOrNum = string | number;
+let greet: Function;
 
-const logDetails = (uid: StringOrNum) => {
-    console.log(uid)
+// example 1
+let greet1:() => void;
+
+// example 2
+let greet2: (a: string, b:string) => void;
+greet = (name: string, greeting:string) => {
+    console.log(`${name} ${greeting}`);
+}
+
+// example 3
+let calc: (a: number, b: number, c: string) => number;
+
+calc = (numOne: number,  numTwo: number, action: string) => {
+    if (action === 'add') {
+        return numOne + numTwo;
+    }
+    return numOne - numTwo;
+}
+
+// example 4
+let logDetails: (obj: {name: string, age: number}) => void;
+
+type person = {name: string, age: number};
+
+logDetails = (human: person) => {
+    console.log(`${human.name} is ${human.age} years old`);
 }
