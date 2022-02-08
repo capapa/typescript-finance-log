748c5c8708f2e1ce026a2932f189d05545a9426e
RashRS <rash.rrs@gmail.com>
2022-02-08 14:55:58 +0500

    TS Functions

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log('test');
+let greet;
+greet = () => {
+    console.log('hello!');
+};
+const add = (a, b, c, d = 10) => {
+    console.log(a + b);
+    console.log(c);
+    console.log(d);
+};
+add(5, 7);
+const minus = (a, b) => {
+    return a - b;
+};
+let result = minus(10, 2);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,19 @@
-console.log('test')
+let greet: Function;
+
+greet = () => {
+    console.log('hello!');
+}
+
+const add = (a:number, b:number, c?: number | string, d: number = 10) => {
+    console.log(a + b);
+    console.log(c);
+    console.log(d);
+}
+
+add(5, 7);
+
+const minus = (a:number, b:number) => {
+    return a - b;
+}
+
+let result = minus(10, 2);
